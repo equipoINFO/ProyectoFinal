4fa1bbbe3c07e61e6e592f26e0534f46eed606c8
jflumian <joaquin.jf98@hotmail.com>
2022-09-01 19:36:44 -0300

    Heroku config agregado

--- Blog2022/settings/base.py
+++ Blog2022/settings/base.py
@@ -132,8 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static')),
 
-MEDIA = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA = '/static/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
