cd02fe4deeb98c24f8007b288f0f3ab040ecf5fd
jflumian <joaquin.jf98@hotmail.com>
2022-09-02 11:33:52 -0300

    intento mostrar img detalle

--- Blog2022/settings/base.py
+++ Blog2022/settings/base.py
@@ -132,8 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static')),
 
-MEDIA = '/static/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
